a42f1dcca66a118461b7c4dcd394dde81766b307
Todd Smith <n7tms@yahoo.com>
2025-03-04 15:52:05 -0700

    initial testing

--- booktracker.py
+++ booktracker.py
@@ -15,22 +15,24 @@ def get_book_info(isbn):
         data = response.json()
         if "items" in data:
             book = data["items"][0]["volumeInfo"]
-            title = book.get("title", "N/A")
-            authors = ", ".join(book.get("authors", ["Unknown Author"]))
-            published_date = book.get("publishedDate", "N/A")
-            description = book.get("description", "No description available")
-            page_count = book.get("pageCount", "N/A")
-            categories = ", ".join(book.get("categories", ["N/A"]))
-            thumbnail = book.get("imageLinks", {}).get("thumbnail", "No image available")
 
             return {
-                "Title": title,
-                "Authors": authors,
-                "Published Date": published_date,
-                "Description": description,
-                "Page Count": page_count,
-                "Categories": categories,
-                "Thumbnail": thumbnail,
+                "Title": book.get("title", "N/A"),
+                "Subtitle": book.get("subtitle", "N/A"),
+                "Authors": ", ".join(book.get("authors", ["Unknown Author"])),
+                "Publisher": book.get("publisher", "N/A"),
+                "Published Date": book.get("publishedDate", "N/A"),
+                "Description": book.get("description", "No description available"),
+                "Page Count": book.get("pageCount", "N/A"),
+                "Categories": ", ".join(book.get("categories", ["N/A"])),
+                "Average Rating": book.get("averageRating", "N/A"),
+                "Ratings Count": book.get("ratingsCount", "N/A"),
+                "Language": book.get("language", "N/A"),
+                "ISBN-10": next((id["identifier"] for id in book.get("industryIdentifiers", []) if id["type"] == "ISBN_10"), "N/A"),
+                "ISBN-13": next((id["identifier"] for id in book.get("industryIdentifiers", []) if id["type"] == "ISBN_13"), "N/A"),
+                "Thumbnail": book.get("imageLinks", {}).get("thumbnail", "No image available"),
+                "Preview Link": book.get("previewLink", "N/A"),
+                "Info Link": book.get("infoLink", "N/A"),
             }
         else:
             return {"Error": "No book found for this ISBN"}
@@ -38,7 +40,8 @@ def get_book_info(isbn):
         return {"Error": f"API request failed with status code {response.status_code}"}
 
 # Example usage
-isbn = "9780143126560"  # Example ISBN
+# isbn = "9780143126560"  # Example ISBN
+isbn = "1913666522"
 book_info = get_book_info(isbn)
 
 for key, value in book_info.items():
